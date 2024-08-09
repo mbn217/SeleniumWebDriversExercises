2005bd32f5027f009c4f1ec3a4fdd53e54c12bfa
MohamedNheri(US) <nherimohamed@gmail.com>
2024-08-09 14:43:14 -0400

    Adding solution for a task

--- src/test/java/day2_basic_functions_locators/Task1_FindElementByIdTask.java
+++ src/test/java/day2_basic_functions_locators/Task1_FindElementByIdTask.java
@@ -1,5 +1,10 @@
 package day2_basic_functions_locators;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1_FindElementByIdTask {
 
     /**
@@ -9,5 +14,16 @@ public class Task1_FindElementByIdTask {
      * 2. Go to https://www.google.com
      * 3. Find the element for the searchBox by ID
      * 4. Enter "Hello World" in the searchBox
+     * 5. Close the browser
      */
+
+    public static void main(String[] args) {
+
+        WebDriver driver  = new ChromeDriver();
+        driver.get("https://www.google.com");
+        WebElement  searchBox = driver.findElement(By.id("APjFqb"));
+        searchBox.sendKeys("Hello World");
+        driver.quit();
+
+    }
 }
--- src/test/java/day2_basic_functions_locators/Task2_FindElementByClassName.java
+++ src/test/java/day2_basic_functions_locators/Task2_FindElementByClassName.java
@@ -1,5 +1,10 @@
 package day2_basic_functions_locators;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task2_FindElementByClassName {
 
     /**
@@ -10,4 +15,15 @@ public class Task2_FindElementByClassName {
      * 3. Find the element for the searchBox by Class Name
      * 4. Enter "Hello World" in the searchBox
      */
+
+    public static void main(String[] args) {
+
+        WebDriver driver  = new ChromeDriver();
+        driver.get("https://www.yahoo.com");
+        // Find the element for the searchBox by Class Name
+        WebElement searchBox = driver.findElement(By.className("_yb_rxlbl2 _yb_1894tvq  _yb_ajji91   "));
+        searchBox.sendKeys("Hello World");
+
+
+    }
 }
