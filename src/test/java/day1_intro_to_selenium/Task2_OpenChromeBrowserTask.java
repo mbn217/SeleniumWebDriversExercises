e68ed628103b2aa420b3c903e44f19ed9dadb9e1
MohamedNheri(US) <nherimohamed@gmail.com>
2024-08-09 14:38:20 -0400

    Adding solution for a task

--- src/test/java/day1_intro_to_selenium/Task2_OpenChromeBrowserTask.java
+++ src/test/java/day1_intro_to_selenium/Task2_OpenChromeBrowserTask.java
@@ -1,9 +1,22 @@
 package day1_intro_to_selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task2_OpenChromeBrowserTask {
     /**
      * Task 2: Open Chrome Browser
      * 1. Open Chrome browser
      * 2. Go to https://www.amazon.com
+     * 3. Close the browser
      */
+
+    public static void main(String[] args) {
+
+        WebDriver driver  = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+        driver.quit();
+
+
+    }
 }
--- src/test/java/day1_intro_to_selenium/Task3_OpenFirefoxBrowserTask.java
+++ src/test/java/day1_intro_to_selenium/Task3_OpenFirefoxBrowserTask.java
@@ -1,10 +1,23 @@
 package day1_intro_to_selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Task3_OpenFirefoxBrowserTask {
 
     /**
      * Task 3: Open Firefox Browser
      * 1. Open Firefox browser
      * 2. Go to https://www.facebook.com
+     * 3. Close the browser
      */
+
+    public static void main(String[] args) {
+        // 1. Open Firefox browser
+        // 2. Go to https://www.facebook.com
+        // 3. Close the browser
+        WebDriver driver  = new FirefoxDriver();
+        driver.get("https://www.facebook.com");
+        driver.quit();
+    }
 }
