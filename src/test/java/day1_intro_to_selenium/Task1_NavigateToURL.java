d23349d67cf199c23c72366319f8bb1058d113af
MohamedNheri(US) <nherimohamed@gmail.com>
2024-08-09 14:36:45 -0400

    Adding solution for a task

--- src/test/java/day1_intro_to_selenium/Task1_NavigateToURL.java
+++ src/test/java/day1_intro_to_selenium/Task1_NavigateToURL.java
@@ -1,9 +1,23 @@
 package day1_intro_to_selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1_NavigateToURL {
     /**
      * Task 1: Navigate to URL
      * 1. Open Chrome browser
      * 2. Go to https://www.google.com
+     * 3. Close the browser
      */
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.quit();
+
+
+
+    }
 }
