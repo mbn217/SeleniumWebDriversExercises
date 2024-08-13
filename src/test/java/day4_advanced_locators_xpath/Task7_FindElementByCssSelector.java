0dbf283a0cd20a53180bc31c9ae74929f001649d
Mohamed Nheri <nherimohamed@gmail.com>
2024-08-13 00:16:13 -0400

    new task

---
+++ src/test/java/day4_advanced_locators_xpath/Task7_FindElementByCssSelector.java
@@ -0,0 +1,18 @@
+package day4_advanced_locators_xpath;
+
+public class Task7_FindElementByCssSelector {
+
+    /**
+     * Task 7: Find Element By Css Selector
+     * 1. Open Chrome browser
+     * 2. Go to http://sauce-demo.com
+     * 3. Find the element for the "Username" text box by Css Selector
+     * 4. Enter "standard_user" in the "Username" text box
+     * 5. Find the element for the "Password" text box by Css Selector
+     * 6. Enter "secret_sauce" in the "Password" text box
+     * 7. Find the element for the "Login" button by Css Selector
+     * 8. Click on the "Login" button
+     * 9. Close the browser
+     *
+     */
+}
